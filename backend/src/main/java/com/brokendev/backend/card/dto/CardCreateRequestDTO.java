e9147d2237bbc0abd6359bc5e2f4423053574404
brokendeveloper <fluccas09@gmail.com>
2026-02-21 11:30:03 -0300

    refactor(cards): add the anotations of swagger

--- backend/src/main/java/com/brokendev/backend/card/dto/CardCreateRequestDTO.java
+++ backend/src/main/java/com/brokendev/backend/card/dto/CardCreateRequestDTO.java
@@ -1,5 +1,10 @@
 package com.brokendev.backend.card.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
 public record CardCreateRequestDTO(
+        @NotBlank(message = "The holdername is required")
+        @Schema(description = "The holdername of the card", example = "Joseph F. Laurent")
         String holderName
 ) { }
--- backend/src/main/java/com/brokendev/backend/card/dto/CardResponseDTO.java
+++ backend/src/main/java/com/brokendev/backend/card/dto/CardResponseDTO.java
@@ -1,13 +1,28 @@
 package com.brokendev.backend.card.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDate;
 
 public record CardResponseDTO(
+
+        @Schema(description = "Id of created card", example = "133")
         Long id,
+
+        @Schema(description = "The number of card", example = "1234 5678 9999 9999")
         String cardNumber,
+
+        @Schema(description = "The name of owner of this card", example = "Joseph F. Laurent")
         String holderName,
+
+        @Schema(description = "Expiration or date limit of card", example = "99/99/99")
         String expiration,
+
+        @Schema(description = "Information about status of card - Can be blocked or unblocked", example = "Blocked")
         boolean blocked,
+
+        @Schema(description = "Date about creation of card", example = "99/99/99 at 99:00:00")
         LocalDate createdAt
 ) {
 }
